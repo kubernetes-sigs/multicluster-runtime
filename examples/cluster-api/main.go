77d931119f50daab1893ab7a84f0947e4f16ac54
Md Tahsin Rahman <tahsin.rahman@agoda.com>
2026-05-04 16:39:30 +0800

    Update cluster-api provider to v1.13

--- examples/cluster-api/main.go
+++ examples/cluster-api/main.go
@@ -22,7 +22,7 @@ import (
 	"fmt"
 	"os"
 
-	capiv1beta1 "sigs.k8s.io/cluster-api/api/v1beta1"
+	capiv1beta1 "sigs.k8s.io/cluster-api/api/core/v1beta1"
 
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
